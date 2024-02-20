7abbd9b5ee0666d45074099887ecb7811dc59a4c
paabloo06 <117017638+paabloo06@users.noreply.github.com>
2024-02-20 17:41:09 +0100

    Update PL1.sql

--- PL1.sql
+++ PL1.sql
@@ -20,7 +20,17 @@ show block_size;
 Select avg(length(id_camion)) from "Camiones"
 /*Cuestion 3*/
 SELECT Matricula FROM camiones WHERE Kilometros = 200000;
+/*Cuestión 4*/
+CREATE TABLE Camiones2 AS
+  SELECT *
+  FROM Camiones
+  ORDER BY kilometros;
 
+ANALYZE Camiones2;
+SELECT count(*) FROM Camiones2;
+SELECT ceil(pg_total_relation_size('Camiones2') / current_setting('block_size')::numeric) AS numero_bloques;
+SELECT pg_size_pretty(pg_total_relation_size('Camiones2')) AS tamano_tabla;
+/*Cuestion 5*/
 
 \d import;
 ROLLBACK;

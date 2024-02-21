2b0857c4810dce6c5d0edbb6df5792f3e7065442
sergiocamero <159786701+sergiocamero@users.noreply.github.com>
2024-02-21 13:25:55 +0100

    Add files via upload

---
+++ cuestiones.sql
@@ -0,0 +1,54 @@
+/*Cuestion 1*/
+SELECT oid, datname FROM pg_database WHERE datname = 'PL1';
+/*Cuestion 2*/
+show block_size;
+SELECT AVG(id_camion) FROM Camiones;
+SELECT AVG(length(matricula)) FROM Camiones;
+SELECT AVG(length(empresa)) FROM Camiones;
+SELECT AVG(kilometros) FROM Camiones;
+SELECT 
+    CEIL(AVG(LENGTH(id_camion::text))) AS longitud_media_id_camion,
+    CEIL(AVG(LENGTH(kilometros::text))) AS longitud_media_kilometros
+FROM 
+    Camiones;
+SELECT 
+    AVG(1 + FLOOR(LOG10(ABS(id_camion)))) AS longitud_media_id_camion,
+    AVG(CEIL(LOG10(kilometros + 1))) AS longitud_media_kilometros
+FROM 
+    Camiones;
+/*Cuestion 3*/
+SELECT Matricula FROM camiones WHERE Kilometros = 200000;
+/*Cuestión 4*/
+CREATE TABLE Camiones2 AS
+  SELECT *
+  FROM Camiones
+  ORDER BY kilometros;
+
+ANALYZE Camiones2;
+SELECT count(*) FROM Camiones2;
+SELECT ceil(pg_total_relation_size('Camiones2') / current_setting('block_size')::numeric) AS numero_bloques;
+SELECT pg_size_pretty(pg_total_relation_size('Camiones2')) AS tamano_tabla;
+/*Cuestion 5*/
+SELECT Matricula FROM Camiones2 WHERE Kilometros = 200000;
+/*Cuestion 6*/
+DELETE FROM camiones
+WHERE id_camion IN (
+    SELECT id_camion
+    FROM camiones
+    ORDER BY random()
+    LIMIT 2000000
+);
+ANALYZE Camiones;
+SELECT count(*) FROM Camiones;
+SELECT ceil(pg_total_relation_size('Camiones') / current_setting('block_size')::numeric) AS numero_bloques;
+SELECT pg_size_pretty(pg_total_relation_size('Camiones')) AS tamano_tabla;
+/*Cuestion 7*/
+VACUUM FULL Camiones;
+--Este comando realiza una reorganización más agresiva de la tabla y puede liberar más espacio, especialmente después de grandes eliminaciones. Sin embargo, ten en cuenta que VACUUM FULL bloqueará la tabla durante la operación.
+REINDEX TABLE Camiones;
+--Después de la eliminación masiva, es recomendable reconstruir los índices para mejorar el rendimiento de las consultas.
+CLUSTER Camiones USING Camiones_kilometros_idx;
+--Este comando reorganiza físicamente la tabla y sus índices en base al índice proporcionado (Camiones_kilometros_idx en este caso), lo que puede mejorar el rendimiento de las consultas que utilizan este índice.
+ANALYZE Camiones;
+--Es importante volver a ejecutar ANALYZE después de las operaciones anteriores para que las estadísticas de la tabla estén actualizadas y el optimizador de consultas pueda tomar decisiones informadas.
+

689fdc5dbf6a3703bdc99fa9c25dadb906308af4
sergiocamero <159786701+sergiocamero@users.noreply.github.com>
2024-03-01 14:17:55 +0100

    NUEVO

--- PL1.sql
+++ PL1.sql
@@ -7,17 +7,19 @@ BEGIN;
 CREATE TABLE Camiones
 (
     id_camion SERIAL PRIMARY KEY,
-    matricula VARCHAR(10) UNIQUE NOT NULL,
-    empresa TEXT NOT NULL,
+    matricula VARCHAR(8) UNIQUE NOT NULL,
+    empresa VARCHAR(12) NOT NULL,
     kilometros INTEGER
 );
-
 \COPY Camiones FROM 'C:\\Users\\scamero\\Desktop\\UAH\\B.Datos2\\registros_camiones.txt' DELIMITER ',' CSV;
+SELECT empresa FROM Camiones;
+DROP TABLE IF EXISTS Camiones;
 
 /*Cuestion 1*/
 SELECT oid, datname FROM pg_database WHERE datname = 'PL1';
 /*Cuestion 2*/
 SELECT current_setting('block_size') AS block_size FROM pg_class WHERE relname = 'camiones';
+
 SELECT AVG(length(matricula)) FROM Camiones;
 SELECT AVG(length(empresa)) FROM Camiones;
 SELECT 
@@ -25,10 +27,19 @@ SELECT
     CEIL(AVG(LENGTH(kilometros::text))) AS longitud_media_kilometros
 FROM 
     Camiones;
+
+SELECT 
+    (current_setting('block_size')::numeric / (AVG(pg_column_size(matricula)) + AVG(pg_column_size(empresa)) + AVG(pg_column_size(id_camion::text)) + AVG(pg_column_size(kilometros::text)))) AS factor_de_bloque
+FROM 
+    camiones;
+
+
 /*Cuestion 3*/
  SELECT pg_stat_reset();
  SELECT Matricula FROM camiones WHERE Kilometros = 200000;
  SELECT blks_read FROM pg_stat_database WHERE datname = current_database();
+
+ SELECT  pg_total_relation_size('camiones') / pg_relation_size('camiones') AS factor_bloque_medio_real; 
 
 /*Cuestión 4*/
 CREATE TABLE Camiones2 AS
@@ -65,13 +76,30 @@ ANALYZE Camiones;
 --Es importante volver a ejecutar ANALYZE después de las operaciones anteriores para que las estadísticas de la tabla estén actualizadas y el optimizador de consultas pueda tomar decisiones informadas.
 
 /*Cuestion 8*/
-CREATE TABLE Camiones3
+CREATE TABLE camiones3_part_0 PARTITION OF Camiones3 FOR VALUES WITH (MODULUS 20, REMAINDER 0);
+CREATE TABLE camiones3_part_1 PARTITION OF Camiones3 FOR VALUES WITH (MODULUS 20, REMAINDER 1);
+...
+CREATE TABLE camiones3_part_19 PARTITION OF Camiones3 FOR VALUES WITH (MODULUS 20, REMAINDER 19);
+
+CREATE TABLE Camiones3 (
+    id_camion SERIAL PRIMARY KEY,
+    matricula VARCHAR(8) UNIQUE NOT NULL,
+    empresa VARCHAR(12) NOT NULL,
+    kilometros INTEGER
+) PARTITION BY HASH (kilometros);
+
+/*Cuestion 10*/
+DROP TABLE IF EXISTS Camiones;
+DROP TABLE IF EXISTS Camiones2;
+DROP TABLE IF EXISTS Camiones3;
+CREATE TABLE Camiones
 (
     id_camion SERIAL PRIMARY KEY,
-    matricula VARCHAR(10) UNIQUE NOT NULL,
-    empresa TEXT NOT NULL,
+    matricula VARCHAR(8) UNIQUE NOT NULL,
+    empresa VARCHAR(12) NOT NULL,
     kilometros INTEGER
 );
+\COPY Camiones FROM 'C:\\Users\\scamero\\Desktop\\UAH\\B.Datos2\\registros_camiones.txt' DELIMITER ',' CSV;
 
 -- Crear las particiones automáticamente usando un bloque PL/pgSQL
 DO $$ 
